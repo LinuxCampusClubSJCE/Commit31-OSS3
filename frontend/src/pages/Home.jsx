107733e1d1a66b443ae94d30624879aebda9fa74
Prithviraj383 <prithvichidri5@gmail.com>
2026-03-01 13:05:04 +0530

    feat: set up routing with react-router-dom

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,9 @@
+function Home() {
+  return (
+    <div>
+      <h1>Task Board</h1>
+    </div>
+  );
+}
+
+export default Home;
---
+++ frontend/src/pages/TaskDetails.jsx
@@ -0,0 +1,9 @@
+function TaskDetails() {
+  return (
+    <div>
+      <h1>Task Details</h1>
+    </div>
+  );
+}
+
+export default TaskDetails;
